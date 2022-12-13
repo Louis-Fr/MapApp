40d616bd055fad74a7d7f897eac66d809e6648ff
Louis Rey <louisreypro@yahoo.com>
2022-12-13 11:21:41 +0100

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Louis REY on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
